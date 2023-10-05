daee2cbe937952bd1c69b83efb1a148cedba29a1
AlexandreSCorreia <alex.sol.correia@gmail.com>
2023-10-04 23:54:46 -0300

    Generic repository InsertAsync method

---
+++ src/MyUserWebApi.Data/Repository/BaseRepository.cs
@@ -0,0 +1,60 @@
+using Microsoft.EntityFrameworkCore;
+using MyUserWebApi.Data.Context;
+using MyUserWebApi.Domain.Entities;
+using MyUserWebApi.Domain.Interfaces;
+
+namespace MyUserWebApi.Data.Repository
+{
+    public class BaseRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        protected readonly AppDbContext _context;
+        private DbSet<T> _dataset;
+
+        public BaseRepository(AppDbContext context)
+        {
+            this._context = context;
+            this._dataset = _context.Set<T>();
+        }
+        public Task<bool> DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<T> InsertAsync(T item)
+        {
+            try
+            {
+                if(item.Id == Guid.Empty)
+                {
+                    item.Id = Guid.NewGuid();
+                }
+
+                item.CreateAt = DateTime.UtcNow;
+                this._dataset.Add(item);
+                
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {               
+                throw ex;
+            }
+
+            return item;
+        }
+
+        public Task<T> SelectAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> SelectAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> UpdateAsync(T item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
