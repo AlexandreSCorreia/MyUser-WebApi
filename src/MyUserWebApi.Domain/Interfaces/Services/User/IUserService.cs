1351863076a7e5e8a1bdfbfcc075e1dbe67dc49c
AlexandreSCorreia <alex.sol.correia@gmail.com>
2023-10-05 00:43:34 -0300

    Implement UserService interface

---
+++ src/MyUserWebApi.Domain/Interfaces/Services/User/IUserService.cs
@@ -0,0 +1,13 @@
+using MyUserWebApi.Domain.Entities;
+
+namespace MyUserWebApi.Domain.Interfaces.Services.User
+{
+    public interface IUserService
+    {
+        Task<UserEntity> Get(Guid id);
+        Task<IEnumerable<UserEntity>> GetAll();
+        Task<UserEntity> Post(UserEntity user);
+        Task<UserEntity> Put(UserEntity user);
+        Task<bool> Delete(Guid id);
+    }
+}
