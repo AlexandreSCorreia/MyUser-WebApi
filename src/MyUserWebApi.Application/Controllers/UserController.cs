c0c46704e06fc9580fbedff994028ed4351d5918
AlexandreSCorreia <alex.sol.correia@gmail.com>
2023-10-05 01:06:14 -0300

    Add references and create the UsersController

---
+++ src/MyUserWebApi.Application/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyUserWebApi.Application.Controllers
+{
+    public class UserController
+    {
+        
+    }
+}
