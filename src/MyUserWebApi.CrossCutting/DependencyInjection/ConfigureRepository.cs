0ebf32d5e78e870050f0d04f95dd476366e50f17
AlexandreSCorreia <alex.sol.correia@gmail.com>
2023-10-10 23:48:48 -0300

    Tuning dependency injection

--- src/MyUserWebApi.CrossCutting/DependencyInjection/ConfigureRepository.cs
+++ src/MyUserWebApi.CrossCutting/DependencyInjection/ConfigureRepository.cs
@@ -1,8 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using MyUserWebApi.Data.Context;
+using MyUserWebApi.Data.Implementations;
 using MyUserWebApi.Data.Repository;
 using MyUserWebApi.Domain.Interfaces;
+using MyUserWebApi.Domain.Repository;
 
 namespace MyUserWebApi.CrossCutting.DependencyInjection
 {
@@ -11,6 +13,7 @@ namespace MyUserWebApi.CrossCutting.DependencyInjection
         public static void ConfigureDependenciesRepository(IServiceCollection serviceColletion)
         {
             serviceColletion.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
+            serviceColletion.AddScoped<IUserRepository, UserImplementation>();
         
             serviceColletion.AddDbContext<AppDbContext>(options =>
                 options.UseSqlServer("Server=localhost,1450;Initial Catalog=MyApi;MultipleActiveResultSets=true;User ID=SA;Password=Numsey#2022"));
