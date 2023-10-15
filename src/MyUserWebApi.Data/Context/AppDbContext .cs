7ba200f0ff052e4e623063061a915dc490bf2c5f
AlexandreSCorreia <alex.sol.correia@gmail.com>
2023-10-15 12:31:51 -0300

    adding admin registration

--- src/MyUserWebApi.Data/Context/AppDbContext .cs
+++ src/MyUserWebApi.Data/Context/AppDbContext .cs
@@ -13,8 +13,18 @@ namespace MyUserWebApi.Data.Context
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-
             modelBuilder.Entity<UserEntity>( new UserMap().Configure);
+            
+            modelBuilder.Entity<UserEntity>().HasData(
+                new UserEntity
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Administrador",
+                    Email = "admininfo@mail.com",
+                    CreateAt = DateTime.Now,
+                    UpdateAt = DateTime.Now
+                }
+            );
         }
 
     }
